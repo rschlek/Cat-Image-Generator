67ca31f334f850ca8cc12bb39161b818c9ca497e
Rainier Schlekewey <rschlekewey@gmail.com>
2022-11-09 16:47:19 -0800

    updated image_selector

--- image_selector.py
+++ image_selector.py
@@ -1,24 +1,34 @@
-from PIL import Image
 import os
+import random
+from PIL import Image
 
-dirname = os.path.dirname(__file__)
+def __str_append(str, add):
+    str += add
+    return str
+
+cat_dir = __str_append(os.path.dirname(__file__),'\\Cat Images')
 
 class image_selector:
     def __init__(self):
+        
+        # initializing the cat image file path list
         self.cat_q = []
 
+        # looping through directory and getting all images paths
+        for filename in os.listdir(cat_dir):
+            print(type(os.path.join(cat_dir, filename)))
+            self.cat_q.append(os.path.join(cat_dir, filename))
         
-
-        self.rainier_q = []
-
-    '''
-    method to get a rainier image
-    '''
-    def get_rainier_pic(self):
-        pass
+        # shuffling list so that image order is random
+        random.shuffle(self.cat_q)
+            
 
     '''
     method to get a cat image
     '''
     def get_cat_image(self):
-        pass
+        print(self.cat_q)
+        image_path = self.cat_q.pop(0)
+        self.cat_q.append(image_path)
+        print(self.cat_q)
+        return Image.open(image_path)
