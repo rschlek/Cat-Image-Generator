effd17d7023440e6fb48d53f0bebbbe1477aeab0
Rainier Schlekewey <rschlekewey@gmail.com>
2022-11-15 17:52:49 -0800

    Fixed image label

--- cat-image-generator.py
+++ cat-image-generator.py
@@ -31,10 +31,14 @@ class image_selector:
     def get_cat_image(self):
         image_path = self.cat_q.pop(0)
         self.cat_q.append(image_path)
-        image = Image.open(image_path)
-        photo_image = PhotoImage(image)
-        return photo_image
+        return image_path
 
+    '''
+    Method to return the next image's dimensions
+    width,height
+    '''
+    def get_next_image_dims(self):
+        pass
 
 
 
@@ -45,20 +49,43 @@ class gui:
     def __init__(self):
         self.selector = image_selector()
         
+        self.WINDOW_HEIGHT = 1000
+        self.WINDOW_WIDTH = 1000
+
+
+        # Window
         self.root = Tk()
-        self.root.geometry("500x500")
+        self.root.geometry(str(self.WINDOW_HEIGHT) + "x" + str(self.WINDOW_WIDTH))
         self.root.title("Natalie's Cat Picture Program")
         
+
         # cat image
-        self.image_label = Label(self.root, image=self.selector.get_cat_image())
+        first_image = ImageTk.PhotoImage(Image.open(self.selector.get_cat_image()))
+        self.image_label = Label(self.root, image=first_image)
         self.image_label.pack(side=TOP)
         
         # Cat Picture Button
-        self.cat_button = Button(self.root, text="New Cat Picture", command=self.new_picture).pack(side=BOTTOM)
+        self.cat_button = Button(self.root, text="New Cat Picture", command=self.__new_picture).pack(side=BOTTOM)
         
+        # mainloop
         self.root.mainloop()
+
+    '''
+    Method to update the image
+    '''
+    def __new_picture(self):
+        new_image = ImageTk.PhotoImage(Image.open(self.selector.get_cat_image()))
+        self.image_label["image"] = new_image
+        self.image_label.image=new_image
+
+    '''
+    Method to get current window dimensions, next image dimensions, 
+    '''
+    def __resize_image(self):
+        pass
+    
        
-       def new_picture(self):
+        
            
 
 
